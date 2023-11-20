048d251de6cc58a0eb0f83c1a87cb20c6a8fd291
PVSivaji <151379131+PVSivaji@users.noreply.github.com>
2023-11-20 18:19:38 +0530

    Update Array shuffle.java

--- JAVA/Array shuffle.java
+++ JAVA/Array shuffle.java
@@ -1,19 +1,40 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.Scanner;
 
-public class Main {
+public class ShuffleArrayExample {
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4, 5, 6, 7};
-        List<Integer> list = Arrays.asList(arr);
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
 
-        Collections.shuffle(list);
+        // Get the size of the array from the user
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
 
-        // Convert the list back to an array
-        Integer[] shuffledArr = list.toArray(new Integer[0]);
+        // Create an array to store user input values
+        Integer[] array = new Integer[size];
+
+        // Get values from the user
+        System.out.println("Enter the values for the array:");
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter value #" + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        // Convert the array to a list for shuffling
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(array));
+
+        // Shuffle the list
+        Collections.shuffle(arrayList);
+
+        // Convert the shuffled list back to an array
+        Integer[] shuffledArray = arrayList.toArray(new Integer[0]);
 
         // Print the shuffled array
-        System.out.println(Arrays.toString(shuffledArr));
+        System.out.println("Shuffled Array: " + Arrays.toString(shuffledArray));
+
+        // Close the Scanner to avoid resource leaks
+        scanner.close();
     }
 }
-
